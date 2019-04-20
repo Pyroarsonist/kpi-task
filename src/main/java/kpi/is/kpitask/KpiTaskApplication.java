c742ca0c693120c2145b034a372e035c88803f6c
Pyroarsonist <velikiydan@gmail.com>
2019-04-21 01:02:42 +0300

    init commit

---
+++ src/main/java/kpi/is/kpitask/KpiTaskApplication.java
@@ -0,0 +1,13 @@
+package kpi.is.kpitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpiTaskApplication.class, args);
+    }
+
+}
